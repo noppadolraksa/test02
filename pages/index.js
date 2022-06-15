a784aa5bc48726f519ed58cbfe09240da4c4dcc6
bright <noppadol.raksa@gmail.com>
2022-06-15 17:47:57 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Pages from '../layout/pageMain';
+
 
 export default function Home(props) {
   return (
-    <Pages
-      route={props.route}
-      title="Homepage"
-      description="home"
-      images=""
-      // images="/static/images/logo.png"
-      keywords=""
-      // login={props.handleLoginShow}
-      // register={props.handleRegisterShow}
-      titleName={'หน้าหลัก'}
-    >
+  
       <div>
         <h1>Welcome To Next.js</h1>
       </div>
-    </Pages>
+    
   );
 }
